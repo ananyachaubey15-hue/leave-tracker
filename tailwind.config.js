35b3e2f859bac2e5f82996d62ebd5cae269534e1
Ananya Chaubey <ananyachaubey15@gmail.com>
2026-03-06 16:33:41 +0530

    Update tailwind.config.js to add global spacing scale.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        xs: '0.125rem', // 2px
+        sm: '0.25rem',  // 4px
+        md: '0.5rem',   // 8px
+        lg: '1rem',     // 16px
+        xl: '1.5rem',   // 24px
+        '2xl': '2rem',  // 32px
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
